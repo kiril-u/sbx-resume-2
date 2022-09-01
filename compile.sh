4a2ab7854dc1d514dd10b5b46a41a08423f6cebb
Kiril Urivsky <kirilurivsky@gmail.com>
2022-09-01 19:09:49 +0300

    sbxr2 v1.2
    ## compile.sh changes:
    - removes auxiliary and other unnecessary files after
    - including one language page resumes
    ## file name changes
    - export file is renamed to "Resume.pdf"
    - tex files are named sxbr2-langcode.tex
    ## Misc.
    - removed other unnecessary files

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-xelatex -synctex=1 -interaction=nonstopmode sbx-resume-2.1-heb.tex
-xelatex -synctex=1 -interaction=nonstopmode sbx-resume-2.1-eng.tex
-pdfunite sbx-resume-2.1-heb.pdf sbx-resume-2.1-eng.pdf sbx-resume-2.1-main.pdf
+# This is the compilation script for sbx-resume-2 v1.2
+xelatex -synctex=1 -interaction=nonstopmode sbxr2-heb.tex
+xelatex -synctex=1 -interaction=nonstopmode sbxr2-eng.tex
+pdfunite sbxr2-heb.pdf sbxr2-eng.pdf Resume.pdf
+rm sbxr2-heb.pdf sbxr2-eng.pdf 
+rm ./*.aux ./*.log ./*.out ./*.synctex.gz
